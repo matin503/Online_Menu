fbcb750762d739e730a58a4b13e483d5de407216
matin <matinamini503@gmail.com>
2025-01-18 00:37:59 +0330

    new entities and update context

--- Online.Menu.presistanse/OnlineMenuContext.cs
+++ Online.Menu.presistanse/OnlineMenuContext.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Online.Menu.Core.Models;
 
-namespace Online.Menu.presistance
+namespace Online.Menu.presistance;
+
+public class OnlineMenuContext(DbContextOptions options) : DbContext(options)
 {
-    internal class OnlineMenuContext
-    {
-    }
+    public DbSet<ItemModel> ItemModels { get; set; }
 }
